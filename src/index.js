de3f18f92222b69f4584dce367c87eefb4c3ed73
Taylor Reis <reist98822@gmail.com>
2024-12-25 16:18:30 -0300

    install yarn

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
-import App from './App'
+import ReactDOM from 'react-dom'
+
+import Home from '../containers/Home'
 import Globalstyles from './styles/globalstyles'
 
 ReactDOM.render(
   <>
-    <App />
+    <Home />
     <Globalstyles />
   </>,
 
