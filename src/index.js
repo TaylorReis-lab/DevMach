b26aecf23e4b0b204e66df36d0e279447a5658c6
Taylor Reis <reist98822@gmail.com>
2024-12-29 08:57:28 -0300

    fixed directory

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import Home from '../containers/Home'
+import Home from '../src/containers/Home'
 import Globalstyles from './styles/globalstyles'
 
 ReactDOM.render(
